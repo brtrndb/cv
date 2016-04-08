7ea13a013ba55bbe83ff3f786389afbe0543d428
Bertrand B <boyer.bertrand@gmail.com>
2016-04-08 15:59:26 +0200

    Added simple script that generate different versions of the CV.

---
+++ curriculum.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Generate different versions of moderncv CV.
+
+# Files and folders options.
+MODERNCV=BertrandBoyer
+CVTEX=$MODERNCV.tex
+CVPDF=$MODERNCV.pdf
+CVFOLDER=./cv
+NULL=/dev/null
+
+# CV Options.
+IMG_EDUCATION=displayImagesEducation
+IMG_EXPERIENCE=displayImagesExperiences
+
+MISSION_VISIAN=displayMissionsVisian
+MISSION_SII=displayMissionsSII
+MISSION_THALES=displayMissionsThales
+MISSION_NOVACOM=displayMissionsNovacom
+
+SECTION_EDUCATION=displaySectionEducation
+SECTION_EXPERIENCE=displaySectionExperiences
+SECTION_SKILLS=displaySectionSkills
+SECTION_PROJECT=displaySectionProjects
+SECTION_HOBBIES=displaySectionHobbies
+
+IMG_ALL="$IMG_EDUCATION $IMG_EDUCATION"
+MISSION_ALL="$MISSION_VISIAN $MISSION_SII $MISSION_THALES $MISSION_NOVACOM"
+SECTION_ALL="$SECTION_EDUCATION $SECTION_EXPERIENCE $SECTION_SKILLS $SECTION_PROJECT $SECTION_HOBBIES"
+OPTIONS_ALL="$IMG_ALL $MISSION_ALL $SECTION_ALL"
+
+# Functions.
+set_attribute(){
+    sed -i 's/'"$1"'}{\(yes\|no\)/'"$1"'}{'"$2"'/g' $CVTEX;
+}
+
+set_pictures(){
+    for img in $IMG_ALL;
+    do
+	set_attribute $img $1;
+    done
+}
+
+set_missions(){
+    for mission in $MISSION_ALL;
+    do
+	set_attribute $mission $1;
+    done
+}
+
+reset_options(){
+    for opt in $OPTIONS_ALL;
+    do
+	set_attribute $opt "yes"
+    done
+}
+
+
+cv_dir(){
+    if [ ! -d "$CVFOLDER" ];
+    then
+	mkdir $CVFOLDER;
+    fi
+}
+
+make_cv(){
+    make > $NULL 2>&1;
+    cv_dir;
+    mv $CVPDF "$CVFOLDER/$1";
+}
+
+# Script.
+
+echo "Starting script.";
+
+echo -n " > Create moderncv with pictures.";
+set_pictures "yes";
+make_cv "$CVPDF-Pics.pdf";
+echo " Done.";
+
+echo -n " > Create moderncv without pictures.";
+set_pictures "no";
+make_cv "$CVPDF-NoPics.pdf";
+echo " Done.";
+
+echo -n " > Create moderncv with full informations.";
+set_pictures "yes";
+set_missions "yes";
+make_cv "$CVPDF-Full.pdf";
+echo " Done.";
+
+echo -n " > Reset $CVTEX to default.";
+reset_options;
+echo "Done."
+
+echo "Finished.";
